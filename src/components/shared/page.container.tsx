bbc3df805220b444633c8a5682d4496cba11d4d4
Misho <chapidze.misho95@gmail.com>
2023-11-21 08:30:09 +0400

    s

--- src/components/shared/page.container.tsx
+++ src/components/shared/page.container.tsx
@@ -1,4 +1,10 @@
-const PageContainer = ({ children }) => {
+import { ReactNode } from "react";
+
+interface PropsType {
+  children: ReactNode;
+}
+
+const PageContainer = ({ children }: PropsType) => {
   return (
     <div className="w-full min-h-screen flex justify-center ">{children}</div>
   );
--- src/components/shared/page.content.tsx
+++ src/components/shared/page.content.tsx
@@ -1,4 +1,10 @@
-const PageContent = ({ children }) => {
+import { ReactNode } from "react";
+
+interface PropsType {
+  children: ReactNode;
+}
+
+const PageContent = ({ children }: PropsType) => {
   return (
     <div className="w-[327px] sm:w-[689px] lg:w-[1111px] min-h-screen font-jost">
       {children}
