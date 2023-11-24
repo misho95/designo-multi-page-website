e16e5c79390df029433f10f8242d2670327898be
Misho <chapidze.misho95@gmail.com>
2023-11-24 20:54:40 +0400

    sxsxsxsdffff

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
   return (
     <animated.footer
       style={{ ...animatedFooter }}
-      className="w-full bg-Black h-[815px] sm:h-[423px] lg:h-[393px] flex justify-center relative mt-[200px] pt-[200px]"
+      className="w-full bg-Black h-[815px] sm:h-[423px] lg:h-[393px] flex justify-center relative mt-[200px] pt-[250px]"
     >
       <PageAspect
         customStyle={
